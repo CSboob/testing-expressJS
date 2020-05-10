ff8edd1d058e1add5532dfef5ba4669e60053059
CalerSchill <tamthieuxz>
2020-05-10 10:10:39 +0700

    add query parameter to search

--- index.js
+++ index.js
@@ -2,18 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var users1 =[
+    {id: 1, name: 'Huy' },
+    {id: 2, name: 'Thinh' }
+];
+
 app.set('view engine','pug')
 app.set('views', './views');
 
  app.get('/', (req, res) => res.render('index',{
      name: 'AAA'
  }));
+ 
+ app.get('/users',(req, res) => res.render('users/index',{users: users1}));
 
- app.get('/users',(req, res) => res.render('users/index',{
-     users: [
-         {id: 1, name: 'Huy' },
-         {id: 2, name: 'Thinh' }
-     ]
- }));
+ app.get('/users/search', (req, res) => {
+    //  gia tri search cua query
+     var q = req.query.q;
+     // nhung user tim dc ung voi gia tri q
+     var matchedUsers = users1.filter((user) => {
+         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+     })
+     // truy cap users/index cho tham so mang users la matchedUsers
+     res.render('users/index',{
+         users: matchedUsers
+    });
+
+ });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
