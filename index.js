09a7b368b5eda8d078b9fc5657747665e881af62
CalerSchill <tamthieuxz>
2020-05-10 16:34:00 +0700

    add nodemon module --save-dev ko require trong index

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 
 var users1 =[
     {id: 1, name: 'Huy' },
-    {id: 2, name: 'Thinh' }
+    {id: 2, name: 'Thinh' },
+    {id: 3, name: 'Thsds'}
 ];
 
 app.set('view engine','pug')
 app.set('views', './views');
 
- app.get('/', (req, res) => res.render('index',{
+app.get('/', (req, res) => res.render('index',{
      name: 'AAA'
- }));
+}));
  
  app.get('/users',(req, res) => res.render('users/index',{users: users1}));
 
  app.get('/users/search', (req, res) => {
+
     //  gia tri search cua query
      var q = req.query.q;
      // nhung user tim dc ung voi gia tri q
@@ -35,7 +37,8 @@ app.set('views', './views');
 
 //app truy cap vo /users/create res se render file create.pug
  app.get('/users/create', (req, res) => res.render('users/create'));    
-//
+
+//req.body doc key va value cua create user nhap vao push vo users1 sau do res chuyen nguoc ve /users
  app.post('/users/create', (req, res) => {
      users1.push(req.body);
      res.redirect('/users');
