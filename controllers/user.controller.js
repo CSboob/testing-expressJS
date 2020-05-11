74045c00b8dbf109ac0cf4707a7cef7d424d0d1c
CalerSchill <tamthieuxz>
2020-05-12 01:29:15 +0700

    Apply controller to make code clearly

---
+++ controllers/user.controller.js
@@ -0,0 +1,46 @@
+var shortid = require('shortid');
+var bodyParser = require('body-parser');
+
+var db = require('../db');
+
+module.exports.index = (req, res) => {
+        res.render('users/index',{
+            users:db.get('users').value()
+    });
+}
+
+module.exports.search = (req, res) => {
+    //  gia tri search cua query
+     var q = req.query.q;
+     // nhung user tim dc ung voi gia tri q
+     var matchedUsers = db.get('users').value().filter((user) => {
+         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+     })
+     // truy cap users/index cho tham so mang users la matchedUsers
+     res.render('users/index',{
+         users: matchedUsers
+    });
+}
+
+module.exports.create = (req, res) => {
+    res.render('users/create')
+}
+
+module.exports.postCreate = (req, res) => {
+    //tao id random cho user vua tao
+   req.body.id = shortid.generate();
+   //push user do vao db va tro lai  list users
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+}
+
+module.exports.viewId = (req, res) => {
+    var id = req.params.id;
+    //get key users trong db.json theo id dong tren
+    var userViewing = db.get('users').find({id: id}).value();
+ 
+    res.render('users/view',{
+        user: userViewing
+    });
+ }
+ 
