e58841bc183a9a277843ae7bc3c878efa8bcfc92
CalerSchill <tamthieuxz>
2020-05-11 19:19:27 +0700

    add view button of user - using shortid of lowdb to generate random id

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 var bodyParser = require('body-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var shortid = require('shortid');
+
+db = low(adapter);
+db.defaults({ users: []})
+  .write()
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-var users1 =[
-    {id: 1, name: 'Huy' },
-    {id: 2, name: 'Thinh' },
-    {id: 3, name: 'Thsds'}
-];
 
 app.set('view engine','pug')
 app.set('views', './views');
@@ -19,14 +21,16 @@ app.get('/', (req, res) => res.render('index',{
      name: 'AAA'
 }));
  
- app.get('/users',(req, res) => res.render('users/index',{users: users1}));
+ app.get('/users',(req, res) => res.render('users/index',{
+     users: db.get('users').value()
+}));
 
  app.get('/users/search', (req, res) => {
 
     //  gia tri search cua query
      var q = req.query.q;
      // nhung user tim dc ung voi gia tri q
-     var matchedUsers = users1.filter((user) => {
+     var matchedUsers = db.get('users').value().filter((user) => {
          return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
      })
      // truy cap users/index cho tham so mang users la matchedUsers
@@ -36,11 +40,29 @@ app.get('/', (req, res) => res.render('index',{
  });
 
 //app truy cap vo /users/create res se render file create.pug
- app.get('/users/create', (req, res) => res.render('users/create'));    
+ app.get('/users/create', (req, res) => {
+     res.render('users/create')
+});    
 
-//req.body doc key va value cua create user nhap vao push vo users1 sau do res chuyen nguoc ve /users
+ app.get('/users/:id', (req, res) => {
+    var id = req.params.id;
+    //get key users trong db.json theo id dong tren
+    var userViewing = db.get('users').find({id: id}).value();
+    console.log(userViewing);
+    //render userViewing
+    res.render('users/view',{
+        user: userViewing
+    });
+ })
+
+//req.body doc key va value sau do write vao db.json  
+// sau do res chuyen nguoc ve /users
  app.post('/users/create', (req, res) => {
-     users1.push(req.body);
+     //tao id random cho user vua tao
+    req.body.id = shortid.generate();
+
+    //push user do vao db va tro lai  list users
+     db.get('users').push(req.body).write();
      res.redirect('/users');
 });
 
